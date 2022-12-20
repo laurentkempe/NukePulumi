101e6d8166badffbacafbed39e65e0e4c0c3f6b0
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2022-12-20 19:36:52 +0100

    Update infra to deploy to Azure App Service

--- infra/Program.cs
+++ infra/Program.cs
@@ -1,40 +1,41 @@
-﻿using Pulumi;
-using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
+﻿using Pulumi.AzureNative.Resources;
 using System.Collections.Generic;
+using Pulumi;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
+using Deployment = Pulumi.Deployment;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
-    // Create an Azure Resource Group
-    var resourceGroup = new ResourceGroup("resourceGroup");
-
-    // Create an Azure resource (Storage Account)
-    var storageAccount = new StorageAccount("sa", new StorageAccountArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        Sku = new SkuArgs
-        {
-            Name = SkuName.Standard_LRS
-        },
-        Kind = Kind.StorageV2
+    var resourceGroup = new ResourceGroup($"rg-{Deployment.Instance.ProjectName}-{Deployment.Instance.StackName}");  
+  
+    var appServicePlan = new AppServicePlan($"plan-{Deployment.Instance.ProjectName}-{Deployment.Instance.StackName}", new AppServicePlanArgs  
+    {  
+        ResourceGroupName = resourceGroup.Name,  
+        Kind = "App",  
+        Sku = new SkuDescriptionArgs  
+        {  
+            Tier = "Basic",  
+            Name = "B1",  
+        },  
+    });  
+  
+    var appService = new WebApp($"app-{Deployment.Instance.ProjectName}-{Deployment.Instance.StackName}", new WebAppArgs   
+    {   
+        ResourceGroupName = resourceGroup.Name,  
+        ServerFarmId = appServicePlan.Id  
+    });
+    
+    var publishingCredentials = ListWebAppPublishingCredentials.Invoke(new()  
+    {  
+        ResourceGroupName = resourceGroup.Name,  
+        Name = appService.Name  
     });
 
-    var storageAccountKeys = ListStorageAccountKeys.Invoke(new ListStorageAccountKeysInvokeArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        AccountName = storageAccount.Name
-    });
-
-    var primaryStorageKey = storageAccountKeys.Apply(accountKeys =>
-    {
-        var firstKey = accountKeys.Keys[0].Value;
-        return Output.CreateSecret(firstKey);
-    });
-
-    // Export the primary key of the Storage Account
-    return new Dictionary<string, object?>
-    {
-        ["primaryStorageKey"] = primaryStorageKey
+    return new Dictionary<string, object?>  
+    {  
+        ["publishingUsername"] = Output.CreateSecret(publishingCredentials.Apply(c => c.PublishingUserName)),  
+        ["publishingUserPassword"] = Output.CreateSecret(publishingCredentials.Apply(c => c.PublishingPassword)),  
+        ["appServiceName"] = appService.Name  
     };
 });
