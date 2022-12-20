9bc2a67ecb47a6f5462a42213407fcea70399278
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2022-12-20 19:37:38 +0100

    Update Nuke build to publish, provision infra and deploy

--- build/Build.cs
+++ build/Build.cs
@@ -1,23 +1,38 @@
 using System;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Pulumi;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+[GitHubActions(
+    "continuous",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.Push },
+    InvokedTargets = new[] { nameof(Compile) })]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    AbsolutePath SourceDirectory => RootDirectory / "src";  
+    AbsolutePath InfrastructureDirectory => RootDirectory / "infra";  
+    AbsolutePath WebApiPackageDirectory => RootDirectory / "output";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    
+    [Solution("./NukePulumi.sln")] readonly Solution Solution;
 
     public static int Main () => Execute<Build>(x => x.Compile);
 
@@ -28,17 +43,72 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("*/bin", "*/obj").ForEach(DeleteDirectory);  
+            EnsureCleanDirectory(ArtifactsDirectory);  
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(_ => _.SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(_ => _  
+                .SetProjectFile(Solution)  
+                .SetConfiguration(Configuration)  
+                .EnableNoRestore());  
         });
 
+    Target Publish => _ => _  
+        .DependsOn(Clean, Compile)  
+        .Executes(() =>  
+        {  
+            DotNetPublish(_ => _  
+                .SetProject(Solution.GetProject("WebApi"))  
+                .SetConfiguration(Configuration)  
+                .EnableNoBuild()  
+                .SetOutput(WebApiPackageDirectory));  
+  
+            ZipFile.CreateFromDirectory(WebApiPackageDirectory, ArtifactsDirectory / "api.zip");  
+        });
+    
+    Target ProvisionInfra => _ => _  
+        .Description("Provision the infrastructure on Azure")  
+        .Executes(() =>  
+        {  
+            PulumiTasks.PulumiUp(_ => _  
+                .SetCwd(InfrastructureDirectory)  
+                .SetStack("dev")  
+                .EnableSkipPreview());  
+        });
+    
+    Target Deploy => _ => _  
+        .DependsOn(Publish)  
+        .After(ProvisionInfra)  
+        .Executes(async () =>  
+        {  
+            var publishingUsername = GetPulumiOutput("publishingUsername");  
+            var publishingUserPassword = GetPulumiOutput("publishingUserPassword");  
+            var base64Auth = Convert.ToBase64String(Encoding.Default.GetBytes($"{publishingUsername}:{publishingUserPassword}"));  
+  
+            await using var package = File.OpenRead(ArtifactsDirectory / "api.zip");  
+            using var httpClient = new HttpClient();  
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Auth);  
+            await httpClient.PostAsync($"https://{GetPulumiOutput("appServiceName")}.scm.azurewebsites.net/api/zipdeploy",  
+                new StreamContent(package));  
+        });
+    
+    string GetPulumiOutput(string outputName)  
+    {  
+        return PulumiTasks.PulumiStackOutput(_ => _  
+                .SetCwd(InfrastructureDirectory)  
+                .SetPropertyName(outputName)  
+                .EnableShowSecrets()
+                .DisableProcessLogOutput())  
+            .StdToText();  
+    }
 }
